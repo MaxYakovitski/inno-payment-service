244c9eaa6961e72652e07a56817c6fd54bd1bebe
max-yakovitski <m.yakovitski@gmail.com>
2026-08-31 00:59:52 +0300

    delete service annotation because it's not needed

--- src/main/java/com/innowise/paymentservice/repository/PaymentRepositoryImpl.java
+++ src/main/java/com/innowise/paymentservice/repository/PaymentRepositoryImpl.java
@@ -16,9 +16,7 @@ import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
 public class PaymentRepositoryImpl implements PaymentRepositoryCustom {
 
